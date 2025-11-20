33b98d6343847d57e32b6e28331e4644c70346ee
Anastasiys Korobetskaya <akorobetskaya@teamscore.ru>
2025-11-20 12:10:07 +0400

    PasswordValidator init

---
+++ src/main/ru/teamscore/akorobetskaya/passwordvalidator/PasswordValidator.java
@@ -0,0 +1,20 @@
+package ru.teamscore.akorobetskaya.passwordvalidator;
+
+/** Проверка пароля на сложность.
+ * Пароль должен отвечать следующим требованиям:
+ * - не менее 8 символов в длину
+ * - содержит строчные, заглавные буквы и цифры
+ * - не должен совпадать с имененем пользователя
+ */
+public class PasswordValidator {
+    /**
+     * Проверка валидности пароля
+     * @param password пароль
+     * @param userName имя пользователя
+     * @return возвращает true, если пароль отвечает всем требованиям
+     */
+    public static boolean isValidPassword(String password, String userName) {
+
+        throw new UnsupportedOperationException("Method isValidPassword is not implemented yet");
+    }
+}
---
+++ src/tests/ru/teamscore/akorobetskaya/passwordvalidator/PasswordValidatorTest.java
@@ -0,0 +1,7 @@
+package ru.teamscore.akorobetskaya.passwordvalidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+}
